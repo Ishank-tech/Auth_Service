ff0b0b2a11026b9de2a9af44173bea2472548381
Ishank Gupta <ishankgupta94482i@gmail.com>
2023-10-06 15:30:23 +0530

    some changes

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,5 +1,7 @@
 const {User, Role} = require('../models/index');
+const ClientError = require('../utils/client-error');
 const ValidationError = require('../utils/validation-error');
+const { StatusCodes } = require('http-status-codes');
 
 class UserRepository{
     
@@ -46,6 +48,14 @@ class UserRepository{
             const user = await User.findOne({where: {
                 email: userEmail
             }});
+            if(!user){
+                throw new ClientError(
+                    'AttributeNotFound',
+                    'Invalid email sent in the request',
+                    'Please check the email, as there is no record of email',
+                    StatusCodes.NOT_FOUND
+                );
+            }
             return user;
         } catch (error) {
             console.log("Something went wrong on repository layer");
